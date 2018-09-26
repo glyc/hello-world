1fd90391e294321f3fc7e57e8ed4bbaca23563e2
glyc <glyc@losys.co.kr>
2018-09-26 20:49:45 +0900

    1번

---
+++ appClientModule/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+	public static void main(String[] args) {
+		
+		System.out.println("hello");
+		
+	
+	}
+
+	/* (non-Java-doc)
+	 * @see java.lang.Object#Object()
+	 */
+	public Main() {
+		super();
+	}
+
+}
